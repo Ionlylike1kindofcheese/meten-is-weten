57577f82b4afd27721bc0505fd6fd8b003f93660
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-09-20 11:44:32 +0200

    Min en Max

--- meten-is-weten.py
+++ meten-is-weten.py
@@ -3,10 +3,16 @@ b = input("geef getal 2 op= ")
 
 if a > b:
     max = a
+    min = b
     print("a is het grootste getal")
 elif a < b:
     min = a
+    max = b
     print("a is het kleinste getal")
-else a == b:
+else: #ik kon hier niet a == b gebruiken want dat gaf een syntax error
+    min = a
+    max = b
     print("a en b zijn even groot")
-    
+
+print("het minimum is: " + str(min))
+print("het maximum is: " + str(max))
